6bf7ff1d85c874469c4a7ecccb3334017b6c9922
hisashi <115007522+Hisashi9818@users.noreply.github.com>
2022-11-15 20:37:49 +0700

    Update BMI.cpp

--- BMI.cpp
+++ BMI.cpp
@@ -1,30 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int w;
-double h;
+double h,BMI;
 main() {
-    cout<<"Calculate BMI"<<endl;
-    cout<<"Weight ";
-    cin>>w;
-    cout<<endl;
-    cout<<"Height ";
-    cin>>h;
-    cout<<endl;
-    double BMI= w/pow(h,2);
-    ;cout<<"------------------"<<endl;
-    ;cout<<"Your weight is (kg): "<<w<<endl;
-    ;cout<<"Your height is (m) : "<<h<<endl;
-     cout<<"------Result-------"<<endl;
-;cout<<"BMI="; ;cout<<setprecision(1)<<fixed<<round(BMI*10)/10<<endl;
-   
-    if(BMI < 20)
-        cout << "You are thin";
-    else
-        if(BMI >= 20 && BMI <= 30)
-        cout << "You are healthy";
-    else
-        if(BMI > 30)
-        cout << "You are fat";
-    
-    }
+   cin>>w>>h;
+   BMI= w/(h*h);
+   while (true)
+cout<<setprecision(1)<<fixed<<round(BMI*10)/10;
+cout<<endl;
 
+     if(BMI < 20){
+        cout << "Nguoi gay";}
+    else
+        if(BMI >= 20 && BMI <= 30){
+        cout << "Nguoi li tuong";}
+    else
+        if(BMI > 30){
+        cout << "Nguoi beo";}
+
+}
